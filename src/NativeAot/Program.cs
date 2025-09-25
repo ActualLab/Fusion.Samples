713a5c77fb4d16ff40e1fdf0391dcfc044733f59
Alex Yakunin <alex.yakunin@gmail.com>
2025-09-25 09:59:36 -0700

    feat: bump Fusion to 10.5.39, update method usage, and adjust logging formats

--- src/NativeAot/Program.cs
+++ src/NativeAot/Program.cs
@@ -16,7 +16,7 @@ if (RuntimeCodegen.NativeMode != RuntimeCodegenMode.DynamicMethods)
     CodeKeeper.RunActions();
 
 WriteLine($"RuntimeCodegen.Mode: {RuntimeCodegen.Mode}");
-WriteLine($"ArgumentList.DisableGenerics: {ArgumentList.DisableGenerics}");
+WriteLine($"ArgumentList.UseGenerics: {ArgumentList.UseGenerics}");
 var l0 = ArgumentList.New();
 var l2 = ArgumentList.New(1, "s");
 var l10 = ArgumentList.New(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
